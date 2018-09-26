e82205b05f6a9b132e2a8b8ab458a16c97cb34a1
shelleydm <36711327+shelleydm@users.noreply.github.com>
2018-09-27 09:44:08 +1000

    completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+let gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('watch', function(){
+
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+
+    watch('app/index.html', function(){
+        browserSync.reload();
+    })
+
+
+    watch('app/assets/**/*.css', function(){
+        gulp.start('cssInject')
+    })
+})
+
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('app/temp/style/style.css')
+    .pipe(browserSync.stream());
+});
